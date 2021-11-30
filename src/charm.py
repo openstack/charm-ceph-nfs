b8e7c454ae1627b35a544f85cc6a78cb02db0b11
Chris MacNaughton <chris.macnaughton@canonical.com>
2021-11-30 14:37:32 +0100

    Add in Ceph client handling, fix up lint and tests

--- src/charm.py
+++ src/charm.py
@@ -13,11 +13,18 @@ develop a new k8s charm using the Operator Framework:
 """
 
 import logging
+import os
+from pathlib import Path
+import subprocess
 
 from ops.charm import CharmBase
 from ops.framework import StoredState
 from ops.main import main
-from ops.model import ActiveStatus
+# from ops.model import ActiveStatus
+
+import charmhelpers.core.host as ch_host
+import charmhelpers.core.templating as ch_templating
+import interface_ceph_client.ceph_client as ceph_client
 
 import ops_openstack.adapters
 import ops_openstack.core
@@ -58,83 +65,183 @@ class CephClientAdapter(ops_openstack.adapters.OpenStackOperRelationAdapter):
         return self.relation.get_relation_data()['key']
 
 
+class CephNFSAdapters(
+        ops_openstack.adapters.OpenStackRelationAdapters):
+    """Collection of relation adapters."""
+
+    relation_adapters = {
+        'ceph-client': CephClientAdapter,
+    }
+
+
 class CephNfsCharm(CharmBase):
-    """Charm the service."""
+    """Ceph NFS Base Charm."""
 
     _stored = StoredState()
+    PACKAGES = ['nfs-ganesha', 'ceph-common']
 
-    def __init__(self, *args):
-        super().__init__(*args)
-        self.framework.observe(self.on.httpbin_pebble_ready, self._on_httpbin_pebble_ready)
-        self.framework.observe(self.on.config_changed, self._on_config_changed)
-        self.framework.observe(self.on.fortune_action, self._on_fortune_action)
-        self._stored.set_default(things=[])
+    CEPH_CAPABILITIES = [
+        "mds", "allow *",
+        "osd", "allow rw",
+        "mon", "allow r, "
+        "allow command \"auth del\", "
+        "allow command \"auth caps\", "
+        "allow command \"auth get\", "
+        "allow command \"auth get-or-create\""]
 
-    def _on_httpbin_pebble_ready(self, event):
-        """Define and start a workload using the Pebble API.
+    REQUIRED_RELATIONS = ['ceph-client', 'cluster']
 
-        TEMPLATE-TODO: change this example to suit your needs.
-        You'll need to specify the right entrypoint and environment
-        configuration for your specific workload. Tip: you can see the
-        standard entrypoint of an existing container using docker inspect
+    CEPH_CONFIG_PATH = Path('/etc/ceph')
+    GANESHA_CONFIG_PATH = Path('/etc/ganesha')
 
-        Learn more about Pebble layers at https://github.com/canonical/pebble
+    CEPH_GANESHA_CONFIG_PATH = CEPH_CONFIG_PATH / 'ganesha'
+    CEPH_CONF = CEPH_GANESHA_CONFIG_PATH / 'ceph.conf'
+    GANESHA_KEYRING = CEPH_GANESHA_CONFIG_PATH / 'ceph.client.ceph-ganesha.keyring'
+    GANESHA_CONF = GANESHA_CONFIG_PATH / 'ganesha.conf'
+
+    SERVICES = ['nfs-ganesha']
+
+    RESTART_MAP = {
+        str(GANESHA_CONF): SERVICES,
+        str(CEPH_CONF): SERVICES,
+        str(GANESHA_KEYRING): SERVICES}
+
+    release = 'default'
+
+    def __init__(self, framework):
+        super().__init__(framework)
+        # super().register_status_check(self.custom_status_check)
+        logging.info("Using %s class", self.release)
+        self._stored.set_default(
+            is_started=False,
+        )
+        self.ceph_client = ceph_client.CephClientRequires(
+            self,
+            'ceph-client')
+        self.adapters = CephNFSAdapters(
+            (self.ceph_client,),
+            self)
+        self.framework.observe(
+            self.ceph_client.on.broker_available,
+            self.request_ceph_pool)
+        self.framework.observe(
+            self.ceph_client.on.pools_available,
+            self.render_config)
+        self.framework.observe(
+            self.on.config_changed,
+            self.request_ceph_pool)
+        self.framework.observe(
+            self.on.upgrade_charm,
+            self.render_config)
+
+    def config_get(self, key):
+        """Retrieve config option.
+
+        :returns: Value of the corresponding config option or None.
+        :rtype: Any
         """
-        # Get a reference the container attribute on the PebbleReadyEvent
-        container = event.workload
-        # Define an initial Pebble layer configuration
-        pebble_layer = {
-            "summary": "httpbin layer",
-            "description": "pebble config layer for httpbin",
-            "services": {
-                "httpbin": {
-                    "override": "replace",
-                    "summary": "httpbin",
-                    "command": "gunicorn -b 0.0.0.0:80 httpbin:app -k gevent",
-                    "startup": "enabled",
-                    "environment": {"thing": self.model.config["thing"]},
-                }
-            },
-        }
-        # Add intial Pebble config layer using the Pebble API
-        container.add_layer("httpbin", pebble_layer, combine=True)
-        # Autostart any services that were defined with startup: enabled
-        container.autostart()
-        # Learn more about statuses in the SDK docs:
-        # https://juju.is/docs/sdk/constructs#heading--statuses
-        self.unit.status = ActiveStatus()
+        return self.model.config.get(key)
 
-    def _on_config_changed(self, _):
-        """Just an example to show how to deal with changed configuration.
+    @property
+    def pool_name(self):
+        """The name of the default rbd data pool to be used for shares.
 
-        TEMPLATE-TODO: change this example to suit your needs.
-        If you don't need to handle config, you can remove this method,
-        the hook created in __init__.py for it, the corresponding test,
-        and the config.py file.
-
-        Learn more about config at https://juju.is/docs/sdk/config
+        :returns: Data pool name.
+        :rtype: str
         """
-        current = self.config["thing"]
-        if current not in self._stored.things:
-            logger.debug("found a new thing: %r", current)
-            self._stored.things.append(current)
-
-    def _on_fortune_action(self, event):
-        """Just an example to show how to receive actions.
-
-        TEMPLATE-TODO: change this example to suit your needs.
-        If you don't need to handle actions, you can remove this method,
-        the hook created in __init__.py for it, the corresponding test,
-        and the actions.py file.
-
-        Learn more about actions at https://juju.is/docs/sdk/actions
-        """
-        fail = event.params["fail"]
-        if fail:
-            event.fail(fail)
+        if self.config_get('rbd-pool-name'):
+            pool_name = self.config_get('rbd-pool-name')
         else:
-            event.set_results({"fortune": "A bug in the code is worth two in the documentation."})
+            pool_name = self.app.name
+        return pool_name
+
+    @property
+    def client_name(self):
+        return self.app.name
+
+    def request_ceph_pool(self, event):
+        """Request pools from Ceph cluster."""
+        if not self.ceph_client.broker_available:
+            logging.info("Cannot request ceph setup at this time")
+            return
+        try:
+            bcomp_kwargs = self.get_bluestore_compression()
+        except ValueError as e:
+            # The end user has most likely provided a invalid value for
+            # a configuration option. Just log the traceback here, the
+            # end user will be notified by assess_status() called at
+            # the end of the hook execution.
+            logging.warn('Caught ValueError, invalid value provided for '
+                         'configuration?: "{}"'.format(str(e)))
+            return
+        weight = self.config_get('ceph-pool-weight')
+        replicas = self.config_get('ceph-osd-replication-count')
+
+        logging.info("Requesting replicated pool")
+        self.ceph_client.create_replicated_pool(
+            name=self.pool_name,
+            replicas=replicas,
+            weight=weight,
+            **bcomp_kwargs)
+        logging.info("Requesting permissions")
+        self.ceph_client.request_ceph_permissions(
+            self.client_name,
+            self.CEPH_CAPABILITIES)
+
+    def refresh_request(self, event):
+        """Re-request Ceph pools and render config."""
+        self.render_config(event)
+        self.request_ceph_pool(event)
+
+    def render_config(self, event):
+        """Render config and restart services if config files change."""
+        if not self.ceph_client.pools_available:
+            logging.info("Defering setup")
+            event.defer()
+            return
+
+        self.CEPH_GANESHA_PATH.mkdir(
+            exist_ok=True,
+            mode=0o750)
+
+        def daemon_reload_and_restart(service_name):
+            subprocess.check_call(['systemctl', 'daemon-reload'])
+            subprocess.check_call(['systemctl', 'restart', service_name])
+
+        rfuncs = {}
+
+        @ch_host.restart_on_change(self.RESTART_MAP, restart_functions=rfuncs)
+        def _render_configs():
+            for config_file in self.RESTART_MAP.keys():
+                ch_templating.render(
+                    os.path.basename(config_file),
+                    config_file,
+                    self.adapters)
+        logging.info("Rendering config")
+        _render_configs()
+        logging.info("Setting started state")
+        self._stored.is_started = True
+        self.update_status()
+        logging.info("on_pools_available: status updated")
+
+    # def custom_status_check(self):
+    #     """Custom update status checks."""
+    #     if ch_host.is_container():
+    #         return ops.model.BlockedStatus(
+    #             'Charm cannot be deployed into a container')
+    #     if self.peers.unit_count not in self.ALLOWED_UNIT_COUNTS:
+    #         return ops.model.BlockedStatus(
+    #             '{} is an invalid unit count'.format(self.peers.unit_count))
+    #     return ops.model.ActiveStatus()
 
 
-if __name__ == "__main__":
-    main(CephNfsCharm)
+@ops_openstack.core.charm_class
+class CephNFSCharmOcto(CephNfsCharm):
+    """Ceph iSCSI Charm for Octopus."""
+
+    _stored = StoredState()
+    release = 'octopus'
+
+
+if __name__ == '__main__':
+    main(ops_openstack.core.get_charm_class_for_release())
---
+++ unit_tests/test_ceph_nfs_charm.py
@@ -0,0 +1,67 @@
+# Copyright 2021 OpenStack Charmers
+# See LICENSE file for licensing details.
+#
+# Learn more about testing at: https://juju.is/docs/sdk/testing
+
+
+import unittest
+import sys
+
+sys.path.append('lib')  # noqa
+sys.path.append('src')  # noqa
+
+from unittest.mock import patch, Mock
+
+from charm import CephNfsCharm
+# from ops.model import ActiveStatus
+from ops.testing import Harness
+
+with patch('charmhelpers.core.host_factory.ubuntu.cmp_pkgrevno',
+           Mock(return_value=1)):
+    import charm
+
+
+class CharmTestCase(unittest.TestCase):
+
+    def setUp(self, obj, patches):
+        super().setUp()
+        self.patches = patches
+        self.obj = obj
+        self.patch_all()
+
+    def patch(self, method):
+        _m = patch.object(self.obj, method)
+        mock = _m.start()
+        self.addCleanup(_m.stop)
+        return mock
+
+    def patch_all(self):
+        for method in self.patches:
+            setattr(self, method, self.patch(method))
+
+
+class _CephNfsCharm(CephNfsCharm):
+
+    @staticmethod
+    def get_bluestore_compression():
+        return {}
+
+
+class TestCephNFSCharmBase(CharmTestCase):
+
+    PATCHES = [
+        'ch_templating',
+        'os',
+        'subprocess',
+    ]
+
+    def setUp(self):
+        super().setUp(charm, self.PATCHES)
+        self.harness = Harness(
+            _CephNfsCharm,
+        )
+        self.addCleanup(self.harness.cleanup)
+
+    def test_init(self):
+        self.harness.begin()
+        self.assertFalse(self.harness.charm._stored.is_started)
